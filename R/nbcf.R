870157ebfde3e07a6318e2ee3813dfd8d868a5f7
小嶋泰弘 <kojimayasuhiro@keisanki.wireless.yale.internal>
2019-02-07 15:26:58 -0500

    Class name has been changed and mean.bias.vec slot was added

--- R/nbcf.R
+++ R/nbcf.R
@@ -1,4 +1,4 @@
-## nbcf class
+## Nbcf class
 ## Yasuhiro Kojima
 
 ##' Data stroge class which contain the information required for finding change points.
@@ -6,24 +6,28 @@
 ##' This class is a data storage object for calculating change points and changing variate.
 ##' You should firstly apply calculateQt and after either perfectSimulation or estimateMap.
 ##' 
-##' @slot count.mat Object of class \code{"ANY"} This is a countmatrix, whose rows represent time, and columns represent variate.
+##' @slot count.mat This is a sparse countmatrix (dgCMatrix), whose rows represent time, and columns represent variate.
 ##' @slot t.vec This is a numeric vector, whose elements represent observed time corresponding to each row of count matrix.
+##' @slot mean.bias.vec This is a numeric vector, whose elements represent expted bias for mean value of each row.
 ##' @slot params This is a named list of parameters, whose mebers must be "lambda", "r", "alpha" and "beta".
+##' @slot lnb.dict Storage for precomputated log negative binomial probability. This is a instance of  \code{\link{LnbDict}}.
 ##' @slot Qt probability of Yt:n given there is change point in t-1
-##' @slot nb.dict.mat Storage for precomputated negative binomial probability.
 ##' @slot sim.change.point change points simulated from perfectSimulation
 ##' @slot map.change.point change points simulated from estimateMap
 ##' @slot change.variate named list of changed variates for each map.change.points
+##'
+##' @import Matrix
 
-
-nbcf <- setClass(
-  "nbcf",
-  slots = c(
-    count.mat="ANY",
+setClass(
+  "Nbcf",
+  representation(
+    count.mat="dgCMatrix",
     t.vec="vector",
+    mean.bias.vec="vector",
     params="list",
-    Qt="list",
-    nb.dict.mat="ANY",
+    lnb.dict.mat="list",
+    Qt="vector",
+    Pst="matrix",
     sim.change.point = "vector",
     map.change.point = "vector",
     change.variate = "list"
