eaf29d245b62fe8c9aba9b269ac908bc084fb3d9
小嶋泰弘 <kojimayasuhiro@keisanki.wireless.yale.internal>
2019-02-08 10:47:01 -0500

    setupLnbDict do not use count.mat but count.vec

--- R/setupLnbDict.R
+++ R/setupLnbDict.R
@@ -3,16 +3,16 @@
 ##'
 ##' This function is responsible for generating instance of \code{\link{LnbDict}}. This involves calculate negative binomial probability for grid points of count and p. Futhermore, this calculate index conversion for count and p (see details in \code{\link{LnbDict}}.
 ##' @title setupLnbDict
-##' @param count.mat Count matrix. Each row represent each variate. Each column represent each observation
+##' @param count.max Max values of \code{count.mat} in \code{link{Nbcd}}
 ##' @param mean.bias.vec Relative mean bias for each observation 
 ##' @param p.res Approximation resolution of parameter p 
-##' @param count.res Approximation resolution of count.
+##' @param count.res Approximation resolution of count
 ##' @return lnbdict Instance of class \code{\link{LnbDict}}
 ##' @seealso [LnbDict]
 ##' @author Yasuhiro Kojima
 
-setupLnbDict <- function(count.mat, p.res, count.res, mean.bias.vec){
-  count.vec <- calculateCountVec(count.mat, count.res)
+setupLnbDict <- function(count.max, p.res, count.res, mean.bias.vec){
+  count.vec <- calculateCountVec(count.max, count.res)
   count.dict <- calculateCountDict(count.vec)
   p.vec <- calculatePvec(p.res)
   p.dict <- calculatePdict(p.vec, mean.bias)
