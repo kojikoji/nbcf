e2d4d527ef298126f66a8b9066d62269faea7190
小嶋泰弘 <kojimayasuhiro@keisanki.local>
2019-02-08 21:37:43 -0500

    Add comments

--- src/calculate_count_dict.cpp
+++ src/calculate_count_dict.cpp
@@ -1,3 +1,9 @@
+#include <Rcpp.h>
+#include <RcppEigen.h>
+
+using namespace Eigen;
+using namespace Rcpp;
+
 // Calculate count dictionary
 //
 // count dictionary is conversion vector from raw count to approximated count
@@ -5,12 +11,6 @@
 // @param count.vec Interger vector, Selected count grid points
 // @return count.dict
 // @author Yasuhiro Kojima
-
-#include <Rcpp.h>
-#include <RcppEigen.h>
-
-using namespace Eigen;
-using namespace Rcpp;
 
 // [[Rcpp::depends(RcppEigen)]]
 
@@ -21,14 +21,17 @@ Eigen::VectorXi rcpp_calculate_count_dict(const Eigen::Map<Eigen::VectorXi> coun
   int count_max = count_vec.maxCoeff();
   Eigen::VectorXi count_dict(count_max+1);
   // initialize grid bound
+  // We select two grids between which each original values are
   int grid_lower = count_vec(0);
   int grid_upper = count_vec(1);
   int grid_upper_index = 1;
   for(int i = 0; i < count_max+1; i++){
     if(i > grid_upper){
+      // Shift grid
       grid_lower = grid_upper;
       grid_upper = count_vec(++grid_upper_index);
     }
+    // Chose lower or upper grid
     if(i - grid_lower < grid_upper - i){
       count_dict(i) = grid_lower;
     }else{
